627831c6ca3066875cac21ae89f09995b747c966
siam456 <69824675+Siam456@users.noreply.github.com>
2023-02-12 12:45:30 +0600

    email api created

--- src/pages/api/email.js
+++ src/pages/api/email.js
@@ -1,31 +1,12 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { SMTPClient } from "emailjs";
 
-export default async function handler(req, res) {
-  let nodemailer = require("nodemailer");
+const client = new SMTPClient({
+  user: process.env.mail,
+  password: process.env.password,
+  host: "smtp.gmail.com",
+  ssl: true,
+});
 
-  const transporter = nodemailer.createTransport({
-    service: "gmail",
-    host: "smtp.gmail.com",
-
-    auth: {
-      user: "hasibulsiam27@gmail.com",
-      pass: "dtsmsscmiwlznvry",
-    },
-  });
-
-  try {
-    await transporter.sendMail({
-      subject: req.body.subject,
-      html: req.body.body,
-      from: req.body.to,
-      to: process.env.EMAIL,
-    });
-    res.send("Thank you! Email send successfully.");
-  } catch (e) {
-    console.log(e);
-    res.status(400).end(JSON.stringify(e));
-    return;
-  }
-
-  // res.status(200).end(JSON.stringify({ message: "Send Mail" }));
+export default function handler(req, res) {
+  res.status(200).end(JSON.stringify({ message: "Send Mail" }));
 }
