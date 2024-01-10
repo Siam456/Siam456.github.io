21ee7120cab7e8633f42d27d883e6ed0dbcb84fe
Siam456 <hasibulsiam27@gmail.com>
2024-01-10 16:42:52 +0600

    add google analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,8 +2,19 @@ import { SidebarProvider } from "@/context/SidebarContext";
 import "@/styles/globals.css";
 import { Windmill } from "@windmill/react-ui";
 import myTheme from "@/asset/theme/myTheme";
+import { initGA, logPageView } from "../utils/analytics";
+import React from "react";
+React.useLayoutEffect = React.useEffect;
 
 export default function App({ Component, pageProps }) {
+  React.useEffect(() => {
+    if (!window.GA_INITIALIZED) {
+      initGA();
+      window.GA_INITIALIZED = true;
+    }
+    logPageView();
+  }, []);
+
   return (
     <>
       <SidebarProvider>
